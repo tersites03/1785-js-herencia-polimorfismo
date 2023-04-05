a69d0c9cf1b05c0871126e6c819f1a7a1592b380
Jorge Lazzarini <tersites.03@gmail.com>
2023-04-05 10:51:30 -0300

    Se agrego el modulo de cuentas.js para generalizar la creación de cuentas, se debe pensar en como no perder funcionalidad ya que al generealizar se pierde el tipo de cuenta.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import {CuentaCorriente} from './CuentaCorriente.js';
-import {CuentaAhorro} from './CuentaAhorro.js';
+//import {CuentaCorriente} from './CuentaCorriente.js';
+//import {CuentaAhorro} from './CuentaAhorro.js';
+import {Cuenta} from './Cuentas.js';
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+const cuentaDeLeonardo = new Cuenta(cliente, '1', '001', 0);
+const cuentaDeMaria = new Cuenta(cliente2,'2','002', 0);
 
-const cuentaAhorroDeLeonardo = new CuentaAhorro(cliente,'9985','001',1000);
+const cuentaAhorroDeLeonardo = new Cuenta(cliente,'9985','001', 0);
 console.log(cuentaDeLeonardo);
+cuentaDeLeonardo.depositoEnCuenta(700);
+console.log(cuentaDeLeonardo.verSaldo());
+cuentaDeLeonardo.retirarDeCuenta(500);
+console.log(cuentaDeLeonardo.verSaldo());
 console.log(cuentaAhorroDeLeonardo);
-console.log(cliente)
+cuentaAhorroDeLeonardo.depositoEnCuenta(120);
+console.log(cuentaAhorroDeLeonardo.verSaldo());
+cuentaAhorroDeLeonardo.retirarDeCuenta(50);
+console.log(cuentaAhorroDeLeonardo.verSaldo());
+//console.log(cuentaDeMaria);
 
 
 
